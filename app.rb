924145dd1b569cbd87453c70fae0ca6eea9a776f
Francis <fgkiron@gmail.com>
2023-05-15 11:54:35 +0300

    Completed assignment

--- app.rb
+++ app.rb
@@ -21,3 +21,10 @@ def add num1, num2
 end
 
 puts add(1,3) + add(2,2)
+puts "Hello world!"
+
+print "Hello world!"
+
+p [1, 2, 3]
+
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
